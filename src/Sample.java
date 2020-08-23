66972396109f468c566ada4bdeeb599d28006757
denshykk <denshykk@gmail.com>
2020-08-23 21:09:41 +0300

    Removing empty lines

--- src/Sample.java
+++ src/Sample.java
@@ -6,15 +6,11 @@ import java.util.ListIterator;
 public class Sample {
 
     public List<Integer> addRandomNumbers(List<Integer> list, int MILLION) {
-
         for (int i = 0; i < MILLION; i++) {
             int randomNumber = (int) (Math.random() * MILLION) + 1;
             list.add(randomNumber);
-
-
         }
         return list;
-
     }
 
     public List<Integer> getNumbers(List<Integer> list, int ONE_HUNDRED_THOUSANDS) {
@@ -22,6 +18,6 @@ public class Sample {
             int randomNumber = (int) (Math.random() * ONE_HUNDRED_THOUSANDS) + 1;
             list.get(randomNumber);
         }
-            return list;
+        return list;
     }
 }
